192ff0d071a2ca537fdc059c4e1e6d6716de0a31
ANGJustinl <96008766+ANGJustinl@users.noreply.github.com>
2023-02-10 21:33:41 +0800

    添加黑名单检测

--- nonebot_plugin_impact/__init__.py
+++ nonebot_plugin_impact/__init__.py
@@ -2,28 +2,23 @@ import asyncio
 from re import I
 from random import choice
 from nonebot.typing import T_State
-from nonebot.params import CommandArg
 from nonebot import on_command, on_regex
 from nonebot.permission import SUPERUSER
-from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.adapters.onebot.v11 import MessageSegment
 from nonebot.adapters.onebot.v11.permission import GROUP_OWNER, GROUP_ADMIN
 
 from .utils import *
 from .txt2img import txt_to_img
 
-suo = on_command("嗦牛子", priority=20, block=True)
-dajiao = on_regex("^(打胶|开导)$", priority=20, block=True)
-pk = on_command("pk", aliases={"对决"}, rule=rule, priority=20, block=False)
-queryJJ = on_command("查询", priority=20, block=False)
-JJrank = on_command(
-    "jj排行榜", aliases={"jj排名", "jj榜单", "jjrank"}, priority=20, block=True)
+suo = on_command("嗦牛子", priority=20)
+dajiao = on_regex("^(打胶|开导)$", priority=20)
+pk = on_command("pk", aliases={"对决"}, rule=rule, priority=20)
+queryJJ = on_command("查询", priority=20)
+JJrank = on_command("jj排行榜", aliases={"jj排名", "jj榜单", "jjrank"}, priority=20)
 openmodule = on_regex(r"^(开启淫趴|禁止淫趴)", permission=SUPERUSER |
                       GROUP_ADMIN | GROUP_OWNER, flags=I, priority=20, block=True)
-yinPa = on_regex(r"^(日群友|透群友|日群主|透群主|日管理|透管理)",
-                 flags=I, priority=20, block=True)
-queryinjection = on_command(
-    "注入查询", aliases={"摄入查询", "射入查询"}, priority=20, block=True)
-yinpaIntroduce = on_command("淫趴介绍", priority=20, block=True)
+yinPa = on_regex(r"^(日群友|透群友|日群主|透群主|日管理|透管理)", 
+                  flags=I, priority=20, block=True)
 
 
 @pk.handle()
@@ -48,7 +43,7 @@ async def _(event: GroupMessageEvent):
                 {uid: round(userdata[uid] + (random_num/2), 3)})  # 更新userdata
             # 更新userdata
             userdata.update({at: round(userdata[at] - random_num, 3)})
-            write_user_data()    # 写入文件
+            write_data()    # 写入文件
             await pk.finish(f"对决胜利喵, 你的{choice(JJvariable)}增加了{round(random_num/2,3)}cm喵, 对面则在你的阴影笼罩下减小了{random_num}cm喵", at_sender=True)
         else:
             random_num = get_random_num()    # 重新生成一个随机数
@@ -56,7 +51,7 @@ async def _(event: GroupMessageEvent):
                 {uid: round(userdata[uid] - random_num, 3)})  # 更新userdata
             # 更新userdata
             userdata.update({at: round(userdata[at] + random_num/2, 3)})
-            write_user_data()    # 写入文件
+            write_data()    # 写入文件
             await pk.finish(f"对决失败喵, 在对面牛子的阴影笼罩下你的{choice(JJvariable)}减小了{random_num}cm喵, 对面增加了{round(random_num/2,3)}cm喵", at_sender=True)
     else:
         # 谁不在userdata里面, 就创建谁
@@ -64,7 +59,7 @@ async def _(event: GroupMessageEvent):
             userdata.update({uid: 10})   # 创建用户
         if at not in userdata:
             userdata.update({at: 10})    # 创建用户
-        write_user_data()     # 写入文件
+        write_data()     # 写入文件
         del pkCDData[uid]   # 删除CD时间
         await pk.finish(f"你或对面还没有创建{choice(JJvariable)}喵, 咱全帮你创建了喵, 你们的{choice(JJvariable)}长度都是10cm喵", at_sender=True)
 
@@ -74,6 +69,8 @@ async def _(event: GroupMessageEvent):
     if not (await check_group_allow(str(event.group_id))):
         await dajiao.finish(notAllow, at_sender=True)
     uid = event.get_user_id()   # 获取用户id, 类型为str
+    if uid in ImpartBlacklist:
+        await dajiao.finish("此用户已禁用")
     allow = await CD_check(uid)    # CD是否允许打胶
     if not allow:   # 如果不允许打胶, 则返回
         await dajiao.finish(f"你已经打不动了喵, 请等待{round(djCDtime-(time.time() - cdData[uid]),3)}秒后再打喵", at_sender=True)
@@ -82,11 +79,11 @@ async def _(event: GroupMessageEvent):
         random_num = get_random_num()    # 生成一个随机数
         userdata.update(
             {uid: round(userdata[uid] + random_num, 3)})  # 更新userdata
-        write_user_data()    # 写入文件
+        write_data()    # 写入文件
         await dajiao.finish(f"打胶结束喵, 你的{choice(JJvariable)}很满意喵, 长了{random_num}cm喵, 目前长度为{userdata[uid]}cm喵", at_sender=True)
     else:
         userdata.update({uid: 10})   # 创建用户
-        write_user_data()    # 写入文件
+        write_data()    # 写入文件
         del cdData[uid]     # 删除CD时间
         await dajiao.finish(f"你还没有创建{choice(JJvariable)}, 咱帮你创建了喵, 目前长度是10cm喵", at_sender=True)
 
@@ -96,6 +93,8 @@ async def _(event: GroupMessageEvent):
     if not (await check_group_allow(str(event.group_id))):
         await suo.finish(notAllow, at_sender=True)
     uid = event.get_user_id()   # 获取用户id, 类型为str
+    if uid in ImpartBlacklist:
+        await suo.finish("此用户已禁用")
     allow = await suo_CD_check(uid)   # CD是否允许嗦
     if not allow:   # 如果不允许嗦, 则返回
         await suo.finish(f"你已经嗦不动了喵, 请等待{round(suoCDTime-(time.time() - suoCDData[uid]),3)}秒后再嗦喵", at_sender=True)
@@ -106,11 +105,11 @@ async def _(event: GroupMessageEvent):
             random_num = get_random_num()    # 生成一个随机数
             userdata.update(
                 {uid: round(userdata[uid] + random_num, 3)})  # 更新userdata
-            write_user_data()    # 写入文件
+            write_data()    # 写入文件
             await suo.finish(f"你的{choice(JJvariable)}很满意喵, 嗦长了{random_num}cm喵, 目前长度为{userdata[uid]}cm喵", at_sender=True)
         else:   # 如果不在userdata里面
             userdata.update({uid: 10})   # 创建用户
-            write_user_data()    # 写入文件
+            write_data()    # 写入文件
             del suoCDData[uid]     # 删除CD时间
             await suo.finish(f"你还没有创建{choice(JJvariable)}喵, 咱帮你创建了喵, 目前长度是10cm喵", at_sender=True)
     else:
@@ -118,11 +117,11 @@ async def _(event: GroupMessageEvent):
             random_num = get_random_num()    # 生成一个随机数
             # 更新userdata
             userdata.update({at: round(userdata[at] + random_num, 3)})
-            write_user_data()    # 写入文件
+            write_data()    # 写入文件
             await suo.finish(f"对方的{choice(JJvariable)}很满意喵, 嗦长了{random_num}cm喵, 目前长度为{userdata[at]}cm喵", at_sender=True)
         else:
             userdata.update({at: 10})    # 创建用户
-            write_user_data()    # 写入文件
+            write_data()    # 写入文件
             del suoCDData[uid]     # 删除CD时间
             await suo.finish(f"他还没有创建{choice(JJvariable)}喵, 咱帮他创建了喵, 目前长度是10cm喵", at_sender=True)
 
@@ -138,14 +137,16 @@ async def _(event: GroupMessageEvent):
             await queryJJ.finish(f"你的{choice(JJvariable)}目前长度为{userdata[uid]}cm喵", at_sender=True)
         else:
             userdata.update({uid: 10})   # 创建用户
-            write_user_data()    # 写入文件
+            write_data()    # 写入文件
             await queryJJ.finish(f"你还没有创建{choice(JJvariable)}喵, 咱帮你创建了喵, 目前长度是10cm喵", at_sender=True)
     else:   # 如果有at
+        if at in ImpartBlacklist:
+            await queryJJ.finish("此用户已禁用")
         if at in userdata:  # 如果在userdata里面
             await queryJJ.finish(f"他的{choice(JJvariable)}目前长度为{userdata[at]}cm喵", at_sender=True)
         else:
             userdata.update({at: 10})    # 创建用户
-            write_user_data()    # 写入文件
+            write_data()    # 写入文件
             await queryJJ.finish(f"他还没有创建{choice(JJvariable)}喵, 咱帮他创建了喵, 目前长度是10cm喵", at_sender=True)
 
 
@@ -154,6 +155,8 @@ async def _(bot: Bot, event: GroupMessageEvent):
     if not (await check_group_allow(str(event.group_id))):
         await JJrank.finish(notAllow, at_sender=True)
     uid = event.get_user_id()   # 获取用户id, 类型为str
+    if uid in ImpartBlacklist:
+        await JJrank.finish("此用户已禁用")
     rankdata = sorted(userdata.items(),
                       key=lambda x: x[1], reverse=True)   # 排序
     if len(rankdata) < 5:
@@ -163,14 +166,13 @@ async def _(bot: Bot, event: GroupMessageEvent):
     index = [i for i, x in enumerate(rankdata) if x[0] == uid]  # 获取用户排名
     if index == []:   # 如果用户没有创建JJ
         userdata.update({uid: 10})   # 创建用户
-        write_user_data()    # 写入文件
+        write_data()    # 写入文件
         await JJrank.finish(f"你还没有创建{choice(JJvariable)}看不到rank喵, 咱帮你创建了喵, 目前长度是10cm喵", at_sender=True)
-    # top5和end5的信息，然后获取其网名
+    # 获取网名
     top5info = [await bot.get_stranger_info(user_id=int(name[0])) for name in top5]
     last5info = [await bot.get_stranger_info(user_id=int(name[0])) for name in last5]
     top5names = [name["nickname"] for name in top5info]
     last5names = [name["nickname"] for name in last5info]
-    # 构造消息，手搓
     reply = "咱只展示前五名和后五名喵\n"
     top5txt = f"{top5names[0]} ------> {top5[0][1]}cm\n{top5names[1]} ------> {top5[1][1]}cm\n{top5names[2]} ------> {top5[2][1]}cm\n{top5names[3]} ------> {top5[3][1]}cm\n{top5names[4]} ------> {top5[4][1]}cm\n"
     last5txt = f"{last5names[0]} ------> {last5[0][1]}cm\n{last5names[1]} ------> {last5[1][1]}cm\n{last5names[2]} ------> {last5[2][1]}cm\n{last5names[3]} ------> {last5[3][1]}cm\n{last5names[4]} ------> {last5[4][1]}cm\n"
@@ -184,9 +186,11 @@ async def _(bot: Bot, event: GroupMessageEvent):
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     gid = event.group_id  # 群号
     uid = event.user_id   # 获取用户id, 类型为int
+    if uid in ImpartBlacklist:
+        await dajiao.finish("此用户已禁用")
     if not (await check_group_allow(str(gid))):
         await JJrank.finish(notAllow, at_sender=True)
-    allow = await fuck_CD_check(event)  # CD检查是否允许
+    allow = await fuck_CD_check(str(uid))  # CD检查是否允许
     if not allow:
         await yinPa.finish(f"你已经榨不出来任何东西了, 请先休息{round(fuckCDTime-(time.time() - ejaculation_CD[str(uid)]),3)}秒", at_sender=True)
     ejaculation_CD.update({str(uid): time.time()})  # 记录时间
@@ -197,79 +201,43 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     command = args[0]
     # 获取群成员列表
     prep_list = await bot.get_group_member_list(group_id=gid)
-    if "群友" in command:  # 如果发送的命令里面含有群友， 说明在透群友
-        prep_list = [prep.get("user_id", 114514) for prep in prep_list]  # 群友列表
-        target = await get_at(event)    # 获取消息有没有at
-        if target == "寄":              # 没有的话
+    if "群友" in command:
+        prep_list = [prep.get("user_id", 114514) for prep in prep_list]
+        target = await get_at(event)
+        if target == "寄":
             # 随机抽取幸运成员
             prep_list.remove(uid)
             lucky_user = choice(prep_list)
             await yinPa.send(f"现在咱将随机抽取一位幸运裙友\n送给{req_user_card}色色！")
-        else:                           # 有的话lucky user就是at的冷
+        else:
             lucky_user = target
-    elif "群主" in command:  # 如果发送的命令里面含有群主， 说明在透群主
-        for prep in prep_list:  # 循环遍历群成员找到role是owner的角色，
+    elif "群主" in command:
+        for prep in prep_list:
             if prep['role'] == 'owner':
                 lucky_user = prep['user_id']
                 break
-        if int(lucky_user) == uid:      # 如果群主是自己
+        if int(lucky_user) == uid:
             del ejaculation_CD[str(uid)]
             await yinPa.finish("你透你自己?")
         await yinPa.send(f"现在咱将把群主\n送给{req_user_card}色色！")
-    elif "管理" in command:      # 如果发送的命令里面含有管理， 说明在透管理
-        admin_id = []               # 空列表， 放管理的
-        for prep in prep_list:  # 遍历， 找到管理就append
+    elif "管理" in command:
+        admin_id = []
+        for prep in prep_list:
             if prep['role'] == 'admin':
                 group_admin_id = prep['user_id']
                 admin_id.append(group_admin_id)
-        if uid in admin_id:         # 如果自己是管理的话， 移除自己
+        if uid in admin_id:
             admin_id.remove(uid)
-        if admin_id == []:          # 如果没有管理的话, del cd信息， 然后finish
+        if admin_id == []:
             del ejaculation_CD[str(uid)]
             await yinPa.finish("喵喵喵? 找不到群管理!")
-        lucky_user = choice(admin_id)   # random抽取一个管理
+        lucky_user = choice(admin_id)
         await yinPa.send(f"现在咱将随机抽取一位幸运管理\n送给{req_user_card}色色！")
-    # 获取群名片或者网名
     lucky_user_card = await get_user_card(bot, gid, int(lucky_user))
-    # 1--100的随机数， 保留三位
-    ejaculation = round(random.uniform(1, 100), 3)
-    try:                                # 尝试直接写入json， 如果没有说明用户不在， 在except里面json里面新建
-        temp = ejaculation_data[str(
-            lucky_user)][get_today()]["ejaculation"] + ejaculation      # 相加
-        await update_ejaculation(round(temp, 3), str(lucky_user))     # 更新
-    except:
-        await update_ejaculation(ejaculation, str(lucky_user))  # 更新
     await asyncio.sleep(2)  # 休眠2秒, 更有效果
-    # 准备调用api, 用来获取头像椭偏
     url = f"http://q1.qlogo.cn/g?b=qq&nk={lucky_user}&s=640"
-    repo_1 = f"好欸！{req_user_card}({uid})用时{random.randint(1, 20)}秒 \n给 {lucky_user_card}({lucky_user}) 注入了{ejaculation}毫升的脱氧核糖核酸, 当日总注入量为：{get_today_ejaculation(str(lucky_user))}"
-    await yinPa.send(repo_1 + MessageSegment.image(url))  # 结束
-
-
-@queryinjection.handle()
-async def _(event: GroupMessageEvent, args: Message = CommandArg()):
-    if not (await check_group_allow(str(event.group_id))):
-        await JJrank.finish(notAllow, at_sender=True)
-    target = args.extract_plain_text()  # 获取命令参数
-    user_id = event.get_user_id()   # 获取对象qq号， 类型string
-    # 判断带不带at
-    [object_id, replay1] = [await get_at(event), "该用户"] if await get_at(event) != "寄" else [user_id, "您"]
-    ejaculation = 0             # 先初始化0
-    if "历史" in target or "全部" in target:
-        try:
-            date = ejaculation_data[object_id]  # 对象不存在直接输出0
-        except:
-            await queryinjection.finish(f"{replay1}历史总被注射量为0ml")
-        pic_string: str = ""            # 文字， 准备弄成图片
-        for key in date:                # 遍历所有的日期
-            temp = date[key]["ejaculation"]
-            ejaculation += temp             # 注入量求和
-            pic_string += f"{key}\t\t{temp}\n"
-        await queryinjection.finish(f"{replay1}历史总被注射量为{ejaculation}ml"+MessageSegment.image(txt_to_img(pic_string)))
-    # 参数没有历史和全部的情况下， 只输出当天
-    else:
-        ejaculation = get_today_ejaculation(object_id)  # 获取对象当天的注入量
-        await queryinjection.finish(f"{replay1}当日总被注射量为{ejaculation}ml")
+    repo_1 = f"好欸！{req_user_card}({uid})用时{random.randint(1, 20)}秒 \n给 {lucky_user_card}({lucky_user}) 注入了{round(random.uniform(1, 100),3)}毫升的脱氧核糖核酸"
+    await yinPa.send(repo_1 + MessageSegment.image(url))
 
 
 @openmodule.handle()
@@ -298,6 +266,5 @@ async def _(event: GroupMessageEvent, state: T_State):
             await openmodule.finish("功能已禁用喵")
 
 
-@yinpaIntroduce.handle()
-async def _(event:GroupMessageEvent):
-    await yinpaIntroduce.send(MessageSegment.image(await plugin_usage()))
+
+
--- nonebot_plugin_impact/utils.py
+++ nonebot_plugin_impact/utils.py
@@ -3,26 +3,7 @@ import json
 import time
 import random
 import nonebot
-from .txt2img import txt_to_img
-from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot
-
-
-def write_group_data() -> None:
-    """写入群配置"""
-    with open("data/impact/groupdata.json", "w", encoding="utf-8") as f:
-        json.dump(groupdata, f, indent=4)
-
-
-def write_user_data() -> None:
-    """写入用户数据"""
-    with open("data/impact/userdata.json", "w", encoding="utf-8") as f:
-        json.dump(userdata, f, indent=4)
-
-
-def write_ejaculation_data() -> None:
-    """写入注入数据"""
-    with open("data/impact/ejaculation_data.json", "w", encoding="utf-8") as f:
-        json.dump(ejaculation_data, f, indent=4)
+from nonebot.adapters.onebot.v11 import GroupMessageEvent,Bot
 
 
 # 读取用户数据
@@ -33,15 +14,10 @@ else:   # 不存在则创建
     if not os.path.exists("data/impact"):
         os.makedirs("data/impact")  # 创建文件夹
     userdata = {}
-    write_user_data()
-# json结构
-# {
-#     "user_id":int
-# }
+    with open("data/impact/userdata.json", "w", encoding="utf-8") as f:
+        json.dump(userdata, f, indent=4)
 
-
-
-# 读取群配置, 可能有人要问了, 为什么要搞这么多个json, 因为我自己的bot上个数据有多个用户数据, 我懒得合并了
+# 读取群配置
 if os.path.exists("data/impact/groupdata.json"):  # 读取用户数据
     with open("data/impact/groupdata.json", "r", encoding="utf-8") as f:
         groupdata = json.load(f)
@@ -49,32 +25,8 @@ else:   # 不存在则创建
     if not os.path.exists("data/impact"):
         os.makedirs("data/impact")  # 创建文件夹
     groupdata = {}
-    write_group_data()
-# json结构
-# {
-#     "group_id": {
-#         "allow": true|false
-#     }
-# }
-
-
-# 读取用户被精液注入的量, 重构上面的json好麻烦, 新建一个json
-if os.path.exists("data/impact/ejaculation_data.json"):  # 读取数据
-    with open("data/impact/ejaculation_data.json", "r", encoding="utf-8")as f:
-        ejaculation_data = json.load(f)
-else:  # 不存在就创建
-    if not os.path.exists("data/impact"):
-        os.makedirs("data/impact")  # 创建文件夹
-    ejaculation_data = {}
-    write_ejaculation_data()
-# 这种结构, 不想重构也不想新建json了， 所以放宽了这么多
-# {
-#     "user_id": {
-#         "date": {
-#             "ejaculation": 123
-#         }
-#     }
-# }
+    with open("data/impact/groupdata.json", "w", encoding="utf-8") as f:
+        json.dump(groupdata, f, indent=4)
 
 
 async def rule(event: GroupMessageEvent) -> bool:
@@ -82,8 +34,6 @@ async def rule(event: GroupMessageEvent) -> bool:
     msg = event.get_message()
     for msg_seg in msg:
         if msg_seg.type == "at":
-            if msg_seg.data["qq"] == "all":
-                return False
             return True
     return False
 
@@ -93,8 +43,6 @@ async def get_at(event: GroupMessageEvent) -> str:
     msg = event.get_message()
     for msg_seg in msg:
         if msg_seg.type == "at":
-            if msg_seg.data["qq"] == "all":
-                return "寄"
             return str(msg_seg.data["qq"])
     return "寄"
 
@@ -116,13 +64,11 @@ async def suo_CD_check(uid: str) -> bool:
     cd = time.time() - suoCDData[uid] if uid in suoCDData else suoCDTime+1
     return True if cd > suoCDTime else False
 
-
-async def fuck_CD_check(event: GroupMessageEvent) -> bool:
+async def fuck_CD_check(uid:str)->bool:
     """透群友检查"""
-    uid = event.get_user_id()
     cd = time.time() - \
         ejaculation_CD[uid] if uid in ejaculation_CD else fuckCDTime+1
-    return True if (cd > fuckCDTime or event.get_user_id() in nonebot.get_driver().config.superusers) else False
+    return True if cd > fuckCDTime else False
 
 
 async def check_group_allow(gid: str) -> bool:
@@ -133,48 +79,26 @@ async def check_group_allow(gid: str) -> bool:
         return False
 
 
-def get_today() -> str:
-    """获取当前年月日  2023-02-04  """
-    today = time.strftime("%Y-%m-%d", time.localtime())
-    return today
+def write_data() -> None:
+    """写入用户数据"""
+    with open("data/impact/userdata.json", "w", encoding="utf-8") as f:
+        json.dump(userdata, f, indent=4)
 
 
-async def update_ejaculation(ejaculation: float, lucky_user: str) -> None:
-    """更新ejaculation_data数据并且写入json"""
-    if lucky_user in ejaculation_data:
-        target_dict = ejaculation_data[lucky_user]
-        target_dict.update({
-            get_today(): {
-                "ejaculation": ejaculation
-            }
-        })
-        ejaculation_data.update({str(lucky_user):target_dict})
-    else:
-        target_dict = {str(lucky_user): {
-            get_today(): {
-                "ejaculation": ejaculation
-            }}
-        }
-        ejaculation_data.update(target_dict)
-    write_ejaculation_data()
-
-
-def get_today_ejaculation(user_id: str) -> float:
-    """获取当日注入的量"""
-    try:
-        return ejaculation_data[str(user_id)][get_today()]["ejaculation"]  # 尝试获取, json没有的话就返回0
-    except:
-        return 0
-
-
-def get_random_num() -> float:
-    """获取随机数 0.1的概率是1-2随机获取, 剩下0.9是0-1"""
+def get_random_num():
+    """获取随机数"""
     rand_num = random.random()
     rand_num = random.uniform(0, 1) if rand_num > 0.1 else random.uniform(1, 2)
     return round(rand_num, 3)
 
 
-async def get_user_card(bot: Bot, group_id, qid) -> str:
+def write_group_data() -> None:
+    """写入群配置"""
+    with open("data/impact/groupdata.json", "w", encoding="utf-8") as f:
+        json.dump(groupdata, f, indent=4)
+
+
+async def get_user_card(bot: Bot, group_id, qid):
     """返还用户nickname"""
     user_info: dict = await bot.get_group_member_info(group_id=group_id, user_id=qid)
     user_card = user_info["card"]
@@ -182,29 +106,16 @@ async def get_user_card(bot: Bot, group_id, qid) -> str:
         user_card = user_info["nickname"]
     return user_card
 
-async def plugin_usage():
-    return txt_to_img(__usage__)
 
-
-notAllow = "群内还未开启淫趴游戏, 请管理员或群主发送\"开启淫趴\", \"禁止淫趴\"以开启/关闭该功能"  # 未开启该功能的send信息
+notAllow = "群内还未开启淫趴游戏, 请管理员或群主发送\"开启淫趴\", \"禁止淫趴\"以开启/关闭该功能"
 JJvariable = ["牛子", "牛牛", "丁丁", "JJ"]     # JJ变量
 cdData = {}  # 冷却数据
 pkCDData = {}   # pk冷却数据
 suoCDData = {}  # 嗦牛子冷却数据
 ejaculation_CD = {}  # 射精CD
 config = nonebot.get_driver().config       # 获取配置
-djCDtime: int = getattr(config, "djcdtime", 300)     # 打胶冷却时间
-pkCDTime: int = getattr(config, "pkcdtime", 60)     # pk冷却时间
-suoCDTime: int = getattr(config, "suocdtime", 300)     # 嗦牛子冷却时间
+djCDtime:int = getattr(config, "djcdtime", 12000)     # 打胶冷却时间
+pkCDTime:int = getattr(config, "pkcdtime", 60)     # pk冷却时间
+suoCDTime: int = getattr(config, "suocdtime", 12000)     # 嗦牛子冷却时间
 fuckCDTime: int = getattr(config, "fuckcdtime", 3600)     # 透群友冷却时间
-
-
-__usage__ = """指令1: 嗦牛子 (给目标牛牛增加长度, 自己或者他人, 通过艾特选择对象, 没有at时目标是自己)
-指令2: 打胶 | 开导 (给自己牛牛增加长度)
-指令3: pk | 对决 (普通的pk,单纯的random实现输赢, 胜利方获取败方随机数/2的牛牛长度)
-指令4: 查询 (目标牛牛长度, 自己或者他人, 通过艾特选择对象, 没有at时目标是自己)
-指令5: jj排行榜 | jj排名 | jj榜单 | jjrank (字面意思, 输出倒数五位和前五位, 以及自己的排名)
-指令6: 开启淫趴|禁止淫趴 (由管理员 | 群主 | SUPERUSERS开启或者关闭淫趴)
-指令7: 日群友|透群友|日群主|透群主|日管理|透管理  (字面意思, 当使用透群友的时候如果at了人那么直接指定)
-指令8: 注入查询 | 摄入查询 (查询目标被透注入的量，后接(历史|全部), 可查看总被摄入的量, 无艾特的时候是自己, 有at的时候是目标)
-指令9: 淫趴介绍 | 淫趴说明| 淫趴帮助 (输出淫趴插件的命令列表)"""
+ImpartBlacklist: list = getattr(config,"ImpartBlacklist",[])
